e2bf6858fa3af25b50ad9c8f9ee5edeaddee253e
szpytma <szpytmapawel@hotmail.com>
2022-09-07 14:47:59 +0100

    commit

---
+++ src/components/Note.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../styles.css";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>title</h1>
+      <p>paragraph</p>
+    </div>
+  );
+}
+
+export default Note;
