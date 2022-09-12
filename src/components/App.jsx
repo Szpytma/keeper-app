c120f50401c8b77f5a54d917f7fe8979a51c6934
szpytma <szpytmapawel@hotmail.com>
2022-09-12 15:49:37 +0100

    adding mapping fuction

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,12 +3,21 @@ import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import "../styles.css";
+import notes from "../notes";
 
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map((note) => (
+        <Note key={note.key} title={note.title} content={note.content} />
+      ))}
+      {/* {notes.map(function (note) {
+        return (
+          <Note key={note.key} title={note.title} content={note.content} />
+        );
+      })} */}
+      ;
       <Footer />
     </div>
   );
---
+++ src/notes.js
@@ -0,0 +1,28 @@
+const notes = [
+  {
+    key: 1,
+    title: "Delegations",
+    content:
+      "Event Delegation is basically a pattern to handle events efficiently. Instead of adding an event listener to each and every similar element, we can add an event listener to a parent element and call an event on a particular target using the . target property of the event object",
+  },
+  {
+    key: 2,
+    title: "Loops",
+    content:
+      "Loops are used in JavaScript to perform repeated tasks based on a condition. Conditions typically return true or false . A loop will continue running until the defined condition returns false",
+  },
+  {
+    key: 3,
+    title: "Arrays",
+    content:
+      "Arrays are a special type of objects. The typeof operator in JavaScript returns object for arrays. But, JavaScript arrays are best described as arrays. Arrays use numbers to access its elements",
+  },
+  {
+    key: 4,
+    title: "JSON",
+    content:
+      "JSON stands for JavaScript Object Notation JSON is a text format for storing and transporting data JSON is self-describing and easy to understand",
+  },
+];
+
+export default notes;
